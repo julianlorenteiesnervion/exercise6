55a92c6685f450af91ebb9e5236acf3bc812e512
julian.lorente <julian.lorente@106-16.info2>
2024-09-20 14:18:52 +0200

    Ejercicio 6 terminado

---
+++ src/exercise6/Exercise6.java
@@ -0,0 +1,13 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Julián Lorente.");
+		System.out.println("I live in Cantillana, Seville, Spain.");
+		System.out.println("");
+		System.out.println("My phone number is +34 657 78 43 92.");
+
+	}
+
+}
